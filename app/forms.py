930cef44c4f5d718f4895ca116323d56729d661e
TARUN-0101 <82829159+TARUN-0101@users.noreply.github.com>
2021-11-15 20:27:08 +0530

    master

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField, PasswordChangeForm
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm, UsernameField, PasswordChangeForm, PasswordResetForm, SetPasswordForm
 from django.contrib.auth.models import User
 from django.utils.html import strip_spaces_between_tags
 from django.utils.translation import gettext, gettext_lazy as _
@@ -39,3 +39,22 @@ class MyPasswordChangeForm(PasswordChangeForm):
     new_password2 = forms.CharField(label=_("Confirm New Password"),
     strip=False, widget=forms.PasswordInput(attrs=
     {'autocomplete': 'new-password', 'class':'form-control'}))
+
+
+class MyPasswordResetForm(PasswordResetForm):
+    email = forms.EmailField(required=True,max_length=254, widget=forms.EmailInput(attrs={'autocomplete':'email', 
+    'class':'form-control'}))
+
+
+class MySetPasswordForm(SetPasswordForm):
+    new_password1 = forms.CharField(
+        label=_("New password"),
+        widget=forms.PasswordInput(attrs={'placeholder': 'New Password', 'class': 'form-control'}),
+        strip=False,
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+    new_password2 = forms.CharField(
+        label=_("New password confirmation"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={'placeholder': 'Repeat Password', 'class': 'form-control'}),
+    )
